a0401aa0724782fb50c9abcb63806cc917ee29c6
potato dog man <saumil707@gmail.com>
2023-07-01 12:55:18 +0800

    Base project

---
+++ Nparks-way-finder/Nparks_way_finderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nparks_way_finderApp.swift
+//  Nparks-way-finder
+//
+//  Created by Sanjit Anand on 1/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Nparks_way_finderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
